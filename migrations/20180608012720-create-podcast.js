47c5d4cb84220cd23d1111837d2e5ed4b82c9c41
Krissy Conant <kconant@KConant-MacBook-Pro.local>
2018-06-08 17:50:55 -0400

    updated podcast field

--- migrations/20180608012720-create-podcast.js
+++ migrations/20180608012720-create-podcast.js
@@ -19,14 +19,6 @@ module.exports = {
       },
       feedURL: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
