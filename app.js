4e5b7f4a9a5c8850105d82a31375577e6496a98d
kaconant <krissyconant@gmail.com>
2018-06-12 21:27:47 -0400

    jarrods rocking changes

--- app.js
+++ app.js
@@ -4,27 +4,32 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('express-handlebars');
+const dotenv = require('dotenv');
+const setupAuth = require('./auth');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const podcastsRouter = require('./routes/podcasts');
-
+dotenv.load();
 var app = express();
 
 // view engine setup
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+setupAuth(app);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/podcasts', podcastsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
