3703b8e67f1dff7f351825759ec1023e01d8963a
Carlton Freeman <carltonfreeman@Carltons-MBP.hsd1.ga.comcast.net>
2018-06-13 21:32:23 -0400

    form sending star rating, comment and podcast ID

--- public/javascript/podcast.js
+++ public/javascript/podcast.js
@@ -4,17 +4,19 @@ console.log('podcast.js connected!')
 // Submit a new Review
 
 $(function () {
-    console.log($("#reviewForm").val());
-    var reviewRating = $("input[name=rating]:checked").attr('value');
-    var reviewComment = $("#comment").attr('value');
-    var reviewUser = 1;
 
     $('.newCommentBtn').click( (e) => {
 
         e.preventDefault();
 
+        var reviewRating = $( "input[type=radio][name=rating]:checked" ).val();
+        var reviewComment = $( "input[type=comment][name=comment]" ).val();
+        var reviewUser = 1;
+
         let data = { "rating": reviewRating, "comment": reviewComment, "UserId": reviewUser };
         let podcastId = $(".podcastInfo").data("id");
+
+        // Make ajax request to post form data to podcasts/:id/reviews
         $.ajax({
             type: "POST",
             url: `http://localhost:3000/podcasts/${podcastId}/reviews`,
