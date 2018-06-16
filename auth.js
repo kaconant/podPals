f8adbd64dd7d34bcccf67175f10fdf1f2839a0fa
jarrodt123 <33099532+jarrodt123@users.noreply.github.com>
2018-06-16 07:11:12 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -19,7 +19,7 @@ const setupAuth = (app) => {
     clientSecret: '1fe954bdd09fc2ec2a23a1a9cdf960290a7908a8',
     callbackURL: 'http://localhost:3000/github/auth'
   }, (accessToken, refreshToken, profile, done) => {
-    models.user.findOrCreate({
+    models.User.findOrCreate({
       where: {
         github_id: profile.id
       }
